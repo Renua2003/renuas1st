86c16ade0935946b492140f7a44fe040b88e5fdd
Renua <renuaajayi@gmail.com>
2023-09-21 15:21:22 -0400

    i just commited this class.

---
+++ src/BuddyInfo.java
@@ -0,0 +1,47 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String number;
+    public BuddyInfo(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public BuddyInfo() {
+        this.name = "Dan";
+        this.address = " 1294 drive ottawa";
+        this.number = "9058123421" ;
+    }
+
+    public String name() {
+        return name;
+    }
+
+
+
+    public String address() {
+        return address;
+    }
+
+
+
+    public String number() {
+        return number;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.!");
+
+        int c =9;
+        int t= 7;
+
+
+        System.out.println( c +t);
+    }
+
+}
+
