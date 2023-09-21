4146d7e844dc6d79ad36ee1b1bea83dff3ccf951
Renua <renuaajayi@gmail.com>
2023-09-21 15:55:25 -0400

    i just commited this class again.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,22 +1,38 @@
 import java.util.ArrayList;
 public class AddressBook {
-    private ArrayList<BuddyInfo> addressholder;
+    ArrayList<BuddyInfo> addresholder;
 
 
     public AddressBook() {
-        ArrayList<BuddyInfo> addresholder = new ArrayList<BuddyInfo>();
+        addresholder = new ArrayList<BuddyInfo>();
     }
 
     public void addBuddy(BuddyInfo var){
-        addressholder.add(var);
+        addresholder.add(var);
     }
 
     public void removeBuddy(BuddyInfo var){
-        addressholder.remove(var);
+        addresholder.remove(var);
 
     }
 
 
+
+
+
     public static void main(String[] args) {
-        System.out.println("hello cruel world" );}
+        System.out.println("hello cruel world" );
+       AddressBook address1 = new AddressBook();
+       BuddyInfo buddy1 = new BuddyInfo("Bola","dry lane Avenue","21");
+       BuddyInfo buddy2 = new BuddyInfo("Tunde","happines lane ","2");
+       address1.addBuddy(buddy2);
+       address1.addBuddy(buddy1);
+
+
+
+
+
+    }
+
+
 }
